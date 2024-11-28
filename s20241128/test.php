75ef557aa660d0dddd8be379fd3cdc4737491d89
youyu <129youyu@gmail.com>
2024-11-28 11:03:43 +0800

    新增javascript_click

---
+++ s20241128/test.php
@@ -0,0 +1,16 @@
+<?php
+$name = 'a-kai';
+echo("Hi $name");
+
+// array
+
+// index array
+$indexArr = [1,2,3];
+
+// associative array
+$assocArr = [
+    's1' => 'amy',
+    's2' => 'bob',
+    's3' => 'cat'
+];
+?>

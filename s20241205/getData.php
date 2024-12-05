76a28933ee506c3717aa672e108834f6c451922e
youyu <129youyu@gmail.com>
2024-12-05 10:51:57 +0800

    js_ajax to php

--- s20241205/getData.php
+++ s20241205/getData.php
@@ -1,3 +1,19 @@
 <?php
-    echo "hello";
-?>
+$data = [
+    's1' => 'amy',
+    's2' => 'bob',
+    's3' => 'cat',
+];
+
+function dd($data)
+{
+    echo "<pre>";
+    print_r($data);
+    echo "</pre>";
+}
+
+// dd($data);
+
+$myJSON = json_encode($data);
+
+echo $myJSON;

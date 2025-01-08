4cb7c3ecef796022fa0751c6c3e56f73cdbfd199
youyu <129youyu@gmail.com>
2025-01-08 13:09:49 +0800

    js_object_table

--- s20250108/s20250108_04_js_object_table.php
+++ s20250108/s20250108_04_js_object_table.php
@@ -1,38 +1,3 @@
-<?php
-function dd($data)
-{
-    echo "<pre>";
-    print_r($data);
-    echo "</pre>";
-}
-
-
-
-$data = [
-    [
-        'id' => 1,
-        'name' => '台北店',
-        'mobile' => '0911-111-111',
-        'address' => '台北市中正路1號'
-    ],
-    [
-        'id' => 2,
-        'name' => '台中店',
-        'mobile' => '0922-222-222',
-        'address' => '台中市中正路2號'
-    ],
-    [
-        'id' => 3,
-        'name' => '高雄店',
-        'mobile' => '0933-333-333',
-        'address' => '高雄市中正路3號'
-    ]
-];
-
-// dd($data);
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -49,7 +14,7 @@ $data = [
     <div class="container mt-3">
         <h2>Data Table</h2>
 
-        <table class="table table-bordered">
+        <table class="table table-bordered" id="myTable">
             <thead>
                 <tr>
                     <th>id</th>
@@ -59,41 +24,90 @@ $data = [
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($data as $key => $value) : ?>
-                <tr>
-                    <td><?= $value['id']; ?></td>
-                    <td><?= $value['name']; ?></td>
-                    <td><?= $value['mobile']; ?></td>
-                    <td><?= $value['address']; ?></td>
-                </tr>
-                <?php endforeach ?>
-
+                <!-- <tr>
+                    <td>2</td>
+                    <td>台中店</td>
+                    <td>0922</td>
+                    <td>台中市中正路2號</td>
+                </tr> -->
             </tbody>
         </table>
     </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.js"
+        integrity="sha512-7Pi/otdlbbCR+LnW+F7PwFcSDJOuUJB3OxtEHbg4vSMvzvJjde4Po1v4BR9Gdc9aXNUNFVUY+SK51wWT8WF0Gg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
     <script>
-    let myObj = [{
-            'id': '1',
-            'name': '',
-            'mobile': '0911-111-111',
-            'address': '高雄市中正路3號'
-        },
-        {
-            'id': '2',
-            'name': '台中店',
-            'mobile': '0922-222-222',
-            'address': '台中市中正路2號'
-        },
-        {
-            'id': '3',
-            'name': '高雄店',
-            'mobile': '0933-333-333',
-            'address': '高雄市中正路3號'
-        },
-    ]
-    console.log('myObj', myObj);
+    $(document).ready(function() {
+        // $.ajax({
+        //      type: "method",
+        //      url: "url",
+        //      data: "data",
+        //      dataType: "dataType",
+        //      success: function (response) {
+        //          data = response.data;
+        //              // foreach append 
+        //      }
+        // });
+
+
+        let data = [{
+                'id': '1',
+                'name': '台北店',
+                'mobile': '0911-111-111',
+                'address': '台北市中正路1號'
+            },
+            {
+                'id': '2',
+                'name': '台中店',
+                'mobile': '0922-222-222',
+                'address': '台中市中正路2號'
+            },
+            {
+                'id': '3',
+                'name': '高雄店',
+                'mobile': '0933-333-333',
+                'address': '高雄市中正路3號'
+            },
+        ]
+        console.log('data', data);
+
+        // 1.bind
+        const myTable = $('#myTable');
+        const myTbody = myTable.find('tbody');
+        console.log('myTable', myTable);
+        console.log('myTbody', myTbody);
+
+        // 2.action
+        // data -> append table
+
+        // 方法一
+        // .class
+        // $(selector).each(function(index,element){
+        // element == this
+        // })
+
+        // 方法二
+        $.each(data, function(key, value) {
+            console.log('key', key);
+            console.log('value', value);
+
+            let tmp = `
+            <tr>
+                <td>${value['id']}</td>
+                <td>${value['name']}</td>
+                <td>${value['mobile']}</td>
+                <td>${value['address']}</td>
+            </tr>    
+            `;
+            myTbody.append(tmp);
+
+        });
+    });
     </script>
+
 
 </body>
 
